2b2824cded76fd2f219cb52f6bcebcb938da9886
davidkc0 <dvid1994@gmail.com>
2016-11-07 13:44:41 -0800

    impoved code

--- pythonText.py
+++ pythonText.py
@@ -1,5 +1,6 @@
 import random
 import sys
+from datetime import datetime
 
 
 #load the file to be read.
@@ -8,8 +9,9 @@ read_it = load_file.read()
 
 # 	print read_it 
 x = raw_input("How many words would you like to be in your sentence? ")
+start_time = datetime.now()
+all_words = read_it.splitlines()
 sentence = ""
-
 #if user input is a number then i equals that number else print error.
 try:
    i = int(x)
@@ -20,7 +22,7 @@ while i > 0:
 	#generate random number between 0 and the last line of the file.
 	random_number = random.randint(0,235886) 
 	#split at the random number line.
-	word = read_it.splitlines()[random_number]
+	word = all_words[random_number]
 	i -= 1
 	#if last word no space else add space.
 	if i == 0:
@@ -29,4 +31,5 @@ while i > 0:
 		sentence += str(word) + " "
 #print sentence with first letter of first word capitalized and period at the end. 
 print sentence.capitalize()+'.'
-
+end_time = datetime.now()
+print end_time - start_time
